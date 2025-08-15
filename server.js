7c709deca5263c6981668008dca9117a9f665bc9
Ayush Shukla <ayushshukla8920@gmail.com>
2025-08-15 19:24:56 +0530

    Added

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
